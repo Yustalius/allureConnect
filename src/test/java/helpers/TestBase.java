1c15bfe1afbc275fe49af175dc75cef5811882bd
Yustalius <deles2013@yandex.ru>
2024-05-15 00:22:17 +0300

    added remote property

--- src/test/java/helpers/TestBase.java
+++ src/test/java/helpers/TestBase.java
@@ -11,11 +11,11 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = "chrome";
+        Configuration.browser = System.getProperty("browser","chrome");
         Configuration.browserSize = System.getProperty("browserSize","1920x1080");
         Configuration.timeout = 10000;
         Configuration.pageLoadTimeout = 100000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("remote","https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
